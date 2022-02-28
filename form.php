0975a8162fed3773b67aed90a1731c26de3b3bf8
saurab10 <sauravsilwal45@gmail.com>
2022-02-28 18:41:27 +0545

    fix:issue pdf and css issue

--- form.php
+++ form.php
@@ -1,3 +1,9 @@
+<form method="POST" enctype="multipart/form-data">
+Select document to upload (only pdf are allowed): <input required type="file" name="pdf" id="pdf" accept="application/pdf">
+<input style="display:block;" type="submit" value="Upload Document" name="submit">
+<div id="success-message">   
+</div>
+</form>
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["document"]["name"]);
@@ -6,7 +12,3 @@ if(isset($_POST["submit"])) {
     print("Thank you. Your file is uploaded.");
 }
 ?>
-<form method="POST" enctype="multipart/form-data">
-Select document to upload (only pdf are allowed): <input type="file" name="document">
-<input type="submit" value="Upload Document" name="submit">
-</form>
